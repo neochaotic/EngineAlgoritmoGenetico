702f472103373bf9f06d3f1d455928d62816d84d
Alisson Rosa <alisson.david.rosa@gmail.com>
2018-01-30 14:43:28 -0200

    Engine 6

---
+++ EngineAlgoritmosGeneticos/Engine5-ColorindoGrafos/colorindoGrafosTest.py
@@ -0,0 +1,99 @@
+import csv, datetime,unittest, genetico
+
+def avalia_aptidao(genes, regras, indiceEstadoLookup):
+    regraAprovada = sum(1 for regra in regras
+                        if regra.EValido(genes, indiceEstadoLookup))
+    return regraAprovada
+
+
+def carrega_dados(nomearquivo):
+    with open(nomearquivo, mode='r') as dentroArquivo:
+        leitor = csv.reader(dentroArquivo)
+        lookup = {linha[0]: linha[1].split(';') for linha in leitor if linha}
+    return lookup
+
+
+class ColorindoGrafostests(unittest.TestCase):
+
+    def test(self):
+        estados = carrega_dados("adjacent_states.csv")
+        regras = contruir_regras(estados)
+        valorOtimo = len(regras)
+        indiceEstadoLookup = {key: index
+                              for index, key in enumerate(sorted(estados))}
+
+        cores = ["laranja", "Amarelo", "Verde", "Azul"]
+        corLookup = {cor[0]: cor for cor in cores}
+        geneset =  list(corLookup.keys())
+
+        iniciaTempo = datetime.datetime.now()
+
+        def fnTela(candidato):
+            tela(candidato, iniciaTempo)
+
+        def fnAptidao(genes):
+            return avalia_aptidao(genes, regras, indiceEstadoLookup)
+
+
+        melhor =  genetico.oMelhor(fnAptidao, len(estados),
+                                   valorOtimo, geneset, fnTela )
+        self.assertTrue(not valorOtimo > melhor.Aptidao)
+
+        chaves = sorted(estados.keys())
+        for indice  in range (len(estados)):
+            print (chaves[indice] + "é" +corLookup[melhor.Genes[indice]])
+
+def tela(candidato, inciaTempo):
+    timeDiff = datetime.datetime.now() - inciaTempo
+    print("{}\t{}\t{}".format(
+        ''.join(map(str, candidato.Genes)),
+        candidato.Aptidao, timeDiff))
+
+
+
+class Regra:
+
+    def __init__(self, no, adjacente):
+        if no < adjacente:
+            no, adjacente = adjacente, no
+        self.No = no
+        self.Adjacente = adjacente
+
+    def __eq__(self, other):
+        return self.No == other.No and self.Adjacente == other.Adjacente
+
+    def __hash__(self):
+        return hash(self.No) *397 ^ hash(self.Adjacente)
+
+    def __str__(self):
+        return self.No + " -> " + self.Adjacente
+
+    def EValido(self, genes, noIndiceLookup):
+        indice = noIndiceLookup[self.No]
+        adjIndice = noIndiceLookup[self.Adjacente]
+        return genes[indice] != genes[adjIndice]
+
+
+def contruir_regras(items):
+    regrasAdd =  {}
+
+    for estado, adjacente in items.items():
+        for estadoadjacente in adjacente:
+            if estadoadjacente == '':
+                continue
+            regra =  Regra(estado, estadoadjacente)
+            if regra in regrasAdd:
+                regrasAdd[regra] += 1
+            else:
+                regrasAdd[regra] = 1
+
+    for k, v in regrasAdd.items():
+        if v != 2:
+            print("Regra {} não é bidirecional".format(k))
+    return regrasAdd.keys()
+
+
+
+
+
+
---
+++ EngineAlgoritmosGeneticos/Engine5-ColorindoGrafos/genetico.py
@@ -0,0 +1,75 @@
+import random
+import statistics
+import time
+import sys
+
+
+def _mutacao(pais, geneSet,avalia_aptidao):
+    indice = random.randrange(0, len(pais.Genes)) #funçao random.range(start,stop,step)
+    geneFilho = pais.Genes[:]
+    novoGene, alternar = random.sample(geneSet, 2) #atribuição multipla de variaveis
+    geneFilho[indice] = alternar \
+        if novoGene == geneFilho[indice] \
+            else novoGene
+    aptidao = avalia_aptidao(geneFilho)
+    return Cromossomo(geneFilho, aptidao)
+
+def _gerar_pais(tamanho, geneSet, avalia_aptidao):
+    genes = []
+    while len(genes) < tamanho:
+        tamanhoAmostra = min(tamanho - len(genes), len(geneSet))
+        genes.extend(random.sample(geneSet, tamanhoAmostra))
+    aptidao = avalia_aptidao(genes)
+    return Cromossomo(genes, aptidao)
+
+def _obter_melhora(novo_filho, gerar_pais):
+    melhorPai = gerar_pais()
+    yield melhorPai
+    while True:
+        filho = novo_filho(melhorPai)
+        if melhorPai.Aptidao > filho.Aptidao:
+            continue
+        if not filho.Aptidao > melhorPai.Aptidao:
+            melhorPai = filho
+            continue
+        yield filho
+        melhorPai = filho
+
+def oMelhor(avaliaAptidao, tamanhoAlvo, aptidaoOtima, geneSet, tela):
+    random.seed()
+
+    def fnMutacao(pais):
+        return _mutacao(pais, geneSet, avaliaAptidao)
+    def fnGerarPais():
+        return _gerar_pais(tamanhoAlvo, geneSet, avaliaAptidao)
+
+    for melhora  in _obter_melhora(fnMutacao, fnGerarPais):
+        tela(melhora)
+        if not aptidaoOtima > melhora.Aptidao:
+            return melhora
+
+class Cromossomo:
+    def __init__(self, genes, aptidao,):
+        self.Genes = genes
+        self.Aptidao = aptidao
+
+
+
+class Benchmark:
+    @staticmethod
+    def run(funcao):
+        cronometragem = []
+        stdout = sys.stdout
+        for i in range(100):
+            sys.stdout = None
+            iniciaTempo = time.time()
+            funcao()
+            segundos = time.time() - iniciaTempo
+            sys.stdout = stdout
+            cronometragem.append(segundos)
+            media = statistics.mean(cronometragem)
+            if i < 10 or i % 10 == 9:
+                print("{} {:3.2f} {:3.2f}".format(
+                    1 + i, media,
+                    statistics.stdev(cronometragem,media)
+                    if i > 1 else 0))
